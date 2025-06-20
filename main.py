e3838d9de118f63b1cd5f2dfc31b226bfc5ef124
shivamo2i <shivamo2i123@gmail.com>
2025-06-20 06:17:16 +0530

    Step 3: Train logistic regression model

--- main.py
+++ main.py
@@ -22,3 +22,10 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
+
+
+from sklearn.linear_model import LogisticRegression
+
+# Step 3: Train logistic regression model
+model = LogisticRegression()
+model.fit(X_train, y_train)
